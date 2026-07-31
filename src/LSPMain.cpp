a747ce2611259b6f78b93597841711bf873e2dce
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-08-01 00:46:50 +0300

    refactor: Remove --docs argument, server is self-contained

--- src/LSPMain.cpp
+++ src/LSPMain.cpp
@@ -1,20 +1,11 @@
 #include "LSP.h"
 #include <iostream>
 
-int main(int argc, char **argv) {
-    // Disable synchronization with C I/O to improve performance
+int main() {
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(NULL);
 
-    std::string docsPath = "src/lsp/native_docs.json";
-    for (int i = 1; i < argc; ++i) {
-        if (std::string(argv[i]) == "--docs" && i + 1 < argc) {
-            docsPath = argv[i + 1];
-            break;
-        }
-    }
-
-    trypillia::LSPServer server(docsPath);
+    trypillia::LSPServer server;
     server.run();
 
     return 0;
